9a708a19fe8c9ed2e6897f544f4ee75b7fb5016a
Tera Kirk <terakrk@gmail.com>
2023-08-20 16:36:23 -0500

    Random color generator works

--- script.js
+++ script.js
@@ -30,6 +30,36 @@ const container = document.querySelector('.canvas');
             color.classList.remove('black');
         });
     }
+
+    const rainbow = document.querySelector('#rainbow');
+    rainbow.addEventListener('click', randomColors);
+
+    function randomColors() {
+        num1 = prompt('First number?', '16');
+        num1 = parseInt(num1);
+      num2 = prompt('Second number?', '16');
+        num2 = parseInt(num2);
+    const sum = num1 * num2;
+    for (let i = 0; i <= sum; i++) {
+    const item = document.createElement('div');
+        item.classList.add('item');
+        container.appendChild(item);
+        item.addEventListener(
+            "mouseenter", 
+            (e) => {
+                e.target.style.backgroundColor = randomRgbColor();
+            }
+        )
+            }
+        
+        
+    };
+function randomRgbColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g =  Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256)
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+    };
 createGrid();
 
 
