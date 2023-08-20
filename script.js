cfcf4dd7e44d9e7f27d06e070a5f019f3528cc32
Tera Kirk <terakrk@gmail.com>
2023-08-19 19:13:21 -0500

    made button to clear the boarrd

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.canvas');
 
 
    function createGrid(num1, num2) {
+      num1 = prompt('First number?', '16');
+        num1 = parseInt(num1);
+      num2 = prompt('Second number?', '16');
+        num2 = parseInt(num2);
     const sum = num1 * num2;
     for (let i = 0; i <= sum; i++) {
     const item = document.createElement('div');
@@ -10,14 +14,23 @@ const container = document.querySelector('.container');
         item.addEventListener(
             "mouseenter", 
             (e) => {
-                e.target.classList.add('color');
+                e.target.classList.add('black');
             }
         )
         
     };
 };
+    const clear = document.querySelector('#clear');
+    clear.addEventListener('click', clearGrid);
 
-createGrid(16, 16);
+    function clearGrid(){
+        const color = document.querySelectorAll('.item');
+        const colorArray = [...color];
+        colorArray.forEach(color => {
+            color.classList.remove('black');
+        });
+    }
+createGrid();
 
 
 
