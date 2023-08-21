190b70d8f4af05d153d5c65d5a21712bd5b83b57
Tera Kirk <terakrk@gmail.com>
2023-08-21 12:09:21 -0500

    got the board to clear altogether rather than adding a new one

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector('.canvas');
 
+const black = document.querySelector('#black');
+black.addEventListener('click', addBlack);
 
-   function createGrid(num1, num2) {
+
+   function addBlack(num1, num2) {
       num1 = prompt('First number?', '16');
         num1 = parseInt(num1);
       num2 = prompt('Second number?', '16');
         num2 = parseInt(num2);
     const sum = num1 * num2;
     for (let i = 0; i <= sum; i++) {
-    const item = document.createElement('div');
+        const item = document.createElement('div');
         item.classList.add('item');
         container.appendChild(item);
         item.addEventListener(
@@ -27,21 +30,21 @@ const container = document.querySelector('.canvas');
         const color = document.querySelectorAll('.item');
         const colorArray = [...color];
         colorArray.forEach(color => {
-            color.classList.remove('black');
+            color.remove();
         });
     }
 
     const rainbow = document.querySelector('#rainbow');
     rainbow.addEventListener('click', randomColors);
 
-    function randomColors() {
+    function randomColors(num1, num2) {
         num1 = prompt('First number?', '16');
         num1 = parseInt(num1);
       num2 = prompt('Second number?', '16');
         num2 = parseInt(num2);
     const sum = num1 * num2;
     for (let i = 0; i <= sum; i++) {
-    const item = document.createElement('div');
+        const item = document.createElement('div');
         item.classList.add('item');
         container.appendChild(item);
         item.addEventListener(
@@ -60,7 +63,6 @@ function randomRgbColor() {
     let b = Math.floor(Math.random() * 256)
     return 'rgb(' + r + ',' + g + ',' + b + ')';
     };
-createGrid();
 
 
 
