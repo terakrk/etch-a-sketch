3daf55db4ea6bff5f9adc5ee7ec8c0da205f6b84
Tera Kirk <terakrk@gmail.com>
2023-08-22 18:07:10 -0500

    each of the buttons clear old grid and create a new one

--- script.js
+++ script.js
@@ -3,33 +3,42 @@ const container = document.querySelector('.canvas');
 const black = document.querySelector('#black');
 black.addEventListener('click', addBlack);
 
+const rainbow = document.querySelector('#rainbow');
+rainbow.addEventListener('click', randomColors);
 
-   function addBlack(num1, num2) {
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearGrid);
+
+
+   function createGrid(num1, num2) {
       num1 = prompt('First number?', '16');
         num1 = parseInt(num1);
       num2 = prompt('Second number?', '16');
         num2 = parseInt(num2);
         if((num1 < 16 || num1 > 100) || (num2 < 16 || num2 > 100)) {
-            alert('The number must be between 16 and 100'); addBlack();
+            alert('The number must be between 16 and 100'); createGrid();
         } else {
             const sum = num1 * num2;
     for (let i = 0; i <= sum; i++) {
         const item = document.createElement('div');
         item.classList.add('item');
         container.appendChild(item);
-        item.addEventListener(
-            "mouseenter", 
-            (e) => {
-                e.target.classList.add('black');
-            }
-        )
-        
+    };
+        };
     };
 
-        };
-        };
-    const clear = document.querySelector('#clear');
-    clear.addEventListener('click', clearGrid);
+    function addBlack() {
+        clearGrid();
+        createGrid();
+        const black = document.querySelectorAll('.item');
+        black.forEach((item) => {
+            item.addEventListener(
+                'mouseenter', function(e) {
+                    e.target.classList.add('black');
+                })
+        });
+    };
+
 
     function clearGrid(){
         const color = document.querySelectorAll('.item');
@@ -37,33 +46,22 @@ black.addEventListener('click', addBlack);
         colorArray.forEach(color => {
             color.remove();
         });
-    }
+    };
 
-    const rainbow = document.querySelector('#rainbow');
-    rainbow.addEventListener('click', randomColors);
 
-    function randomColors(num1, num2) {
-        num1 = prompt('First number?', '16');
-        num1 = parseInt(num1);
-      num2 = prompt('Second number?', '16');
-        num2 = parseInt(num2);
-        if ((num1 < 16 || num1 > 100) || (num2 < 16 || num2 > 100)) {
-            alert('The number must be between 16 and 100'); randomColors();
-        } else {
-        const sum = num1 * num2;
-            for (let i = 0; i <= sum; i++) {
-        const item = document.createElement('div');
-        item.classList.add('item');
-        container.appendChild(item);
-        item.addEventListener(
-            "mouseenter", 
-            (e) => {
+    function randomColors() {
+        clearGrid();
+        createGrid();
+       const colors = document.querySelectorAll('.item');
+        colors.forEach((item) => {
+            item.addEventListener("mouseenter", function (e) {
                 e.target.style.backgroundColor = randomRgbColor();
             }
+        
         )
-            }   
-    };
+    }); 
 };
+
 function randomRgbColor() {
     let r = Math.floor(Math.random() * 256);
     let g =  Math.floor(Math.random() * 256);
